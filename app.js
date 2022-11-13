267b3f3cfc1779255083ce8e18674897799ada9a
AthiraPKumar <athirapkumar16@gmail.com>
2022-11-13 19:34:00 +0530

    initial coomit with bindings to html from data

---
+++ app.js
@@ -0,0 +1,24 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            courseGoalA: 'Finish Course & learn Vue!',
+            courseGoalB: 'Master Course',
+            VueLink: "https://vuejs.org/"
+        };
+    },
+    methods:{
+        outputGoal() {
+            const randomNumber = Math.random();
+            if(randomNumber < 0.5){
+                return this.courseGoalA;
+            } else {
+                return this.courseGoalB;
+            }
+
+        }
+
+    }
+});
+
+
+app.mount('#user-goal')
