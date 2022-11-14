b061ae80f1d1dedd8a83063fb76a1f4d4f9acf1b
AthiraPKumar <athirapkumar16@gmail.com>
2022-11-15 02:53:32 +0530

    dynamic class binding

--- app.js
+++ app.js
@@ -41,11 +41,11 @@ const app = Vue.createApp({
     },
     boxSelected(box) {
       if (box === "A") {
-        this.boxASelected = true;
+        this.boxASelected =  !this.boxASelected;
       } else if (box === "B") {
-        this.boxBSelected = true;
+        this.boxBSelected = !this.boxBSelected;
       } else if (box === "C") {
-        this.boxCSelected = true;
+        this.boxCSelected = !this.boxCSelected;
       }
     },
   },
